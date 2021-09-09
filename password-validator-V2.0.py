637ea4fed0a2062cc9845d4997122071311118da
orel199973 <47865329+orel199973@users.noreply.github.com>
2021-09-09 20:59:04 +0300

    Update password-validator-V2.0.py

--- password-validator-V2.0.py
+++ password-validator-V2.0.py
@@ -12,7 +12,6 @@ colorama.init()
 pswd = sys.argv[1]
 if pswd == "-f":
     pswd=open(sys.argv[2], 'r').read()
-    print(pswd)
 """ Here we define our password policy using regular expressions using the re module """
 reg = "^(?=.[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{10,}"
 match_re = re.compile(reg)
@@ -20,7 +19,8 @@ res = re.search(match_re, pswd)
 
 """ These are the conditions that will check and print us the correct output """
 if res:
+    print(pswd)
     print(Fore.GREEN + "- Succeeded, The password is very strong(0)")
 else:
-    print(
-        Fore.RED + "- Weak password, Be sure to follow this password policy:\n(enter 10+ characters, use special characters, numbers and uppercase and lowercase letters).(1)")
+    print(pswd)
+    print(Fore.RED + "- Weak password, Be sure to follow this password policy:\n(enter 10+ characters, use special characters, numbers and uppercase and lowercase letters).(1)")

3174150663e82b00de7eeb3981b7f0004643ba08
orel199973 <47865329+orel199973@users.noreply.github.com>
2021-09-11 17:52:17 +0300

    Update password-validator-V2.0.py

--- password-validator-V2.0.py
+++ password-validator-V2.0.py
@@ -1,6 +1,6 @@
 # !/usr/bin/python
 
-""" Attracting models that will help us build the script more easily """
+# Attracting models that will help us build the script more easily.
 import sys
 import re
 import colorama
@@ -8,17 +8,17 @@ from colorama import Fore
 
 colorama.init()
 
-""" argv pulls the user input from the outside and puts it into the "pswd" e variable using the sys module """
+# argv pulls the user input from the outside and puts it into the "pswd" e variable using the sys module.
 pswd = sys.argv[1]
-""" This is a test condition if we use a flag. """
+# This is a test condition if we use a flag. 
 if pswd == "-f":
     pswd=open(sys.argv[2], 'r').read()
-""" Here we define our password policy using regular expressions using the re module """
+# Here we define our password policy using regular expressions using the re module.
 reg = "^(?=.[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{10,}"
 match_re = re.compile(reg)
 res = re.search(match_re, pswd)
 
-""" These are the conditions that will check and print us the correct output """
+# These are the conditions that will check and print us the correct output.
 if res:
     print(pswd)
     print(Fore.GREEN + "- Succeeded, The password is very strong(0)")
